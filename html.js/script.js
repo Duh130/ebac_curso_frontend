44bc98d38846efc6488e3e480aca1f7d0dba9821
duh <duka13505@gmail.com>
2024-02-07 16:27:31 -0300

    exercicio_html_js.

---
+++ html.js/script.js
@@ -0,0 +1,27 @@
+function validarFormulario() {
+    event.preventDefault();
+    var valorA = parseInt(document.getElementById('campoA').value);
+    var valorB = parseInt(document.getElementById('campoB').value);
+    if (valorB > valorA) {
+        exibirMensagemPositiva();
+        return true; 
+    } else {
+        exibirMensagemNegativa();
+        return false; 
+    }
+}
+
+
+
+function exibirMensagemPositiva() {
+    var mensagemElement = document.getElementById('mensagem');
+    mensagemElement.innerHTML = 'Formulário válido. B é maior que A.';
+    mensagemElement.style.color = 'green';
+}
+
+function exibirMensagemNegativa() {
+    var mensagemElement = document.getElementById('mensagem');
+    mensagemElement.innerHTML = 'Formulário inválido. B deve ser maior que A.';
+    mensagemElement.style.color = 'red';
+}
+
